6165b8425b9c08df0d4102be6886e675775563bd
RazelRaz <razel16dcm@gmail.com>
2024-03-21 15:12:37 +0600

    Math.max use

---
+++ max2.js
@@ -0,0 +1,10 @@
+const max = 96;
+const jim = 67;
+const den = 98;
+
+function maxNum(num1,num2,num3){
+    return Math.max(num1,num2,num3)
+}
+
+const showMax = maxNum(max, jim, den);
+console.log(showMax);
