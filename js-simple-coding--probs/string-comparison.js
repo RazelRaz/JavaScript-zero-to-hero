88c3c9950d7e8d958112b9c6801584f7c428ab6d
RazelRaz <razel16dcm@gmail.com>
2024-05-07 11:38:50 +0600

    string comparison

---
+++ js-simple-coding--probs/string-comparison.js
@@ -0,0 +1,10 @@
+const userName = "blackPink";
+const userInput = "blacKPink";
+
+
+if(userName.toLowerCase() === userInput.toLowerCase()){
+  console.log('Success');
+  
+} else {
+  console.log('Not a Valid User');
+}
